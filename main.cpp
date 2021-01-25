fa4934e70a9e7a790b90dc95e59eed65cddf9bc1
pereiradaniel <danielpaulpereira@protonmail.com>
2021-01-25 12:50:56 -0500

    Working example.

--- main.cpp
+++ main.cpp
@@ -2,13 +2,18 @@
 #include <string>
 using namespace std;
 
+
 int main()
 {
-	int a[] { 1, 56, -3 };
+	int a[5] { 1, 56, -3 };
+	int b[6];
+
+	int* arr = new int[6]; // int* exact type
 
 	// for (auto i = 0; i < 3; ++i)
 	for (int elem : a) // like for each
 		// cout << a[i] << ", ";
 		cout << elem << ", ";
+		
 		cout << endl;
 }
