a45c799a3a136a7681cb8129c89eaa884ee84704
pereiradaniel <danielpaulpereira@protonmail.com>
2021-01-25 12:44:14 -0500

    for each

--- main.cpp
+++ main.cpp
@@ -4,9 +4,11 @@ using namespace std;
 
 int main()
 {
-	int a[5] { 1, 56, -3 };
+	int a[] { 1, 56, -3 };
 
-	for (auto i = 0; i < 3; ++i)
-		cout << a[i] << ", ";
+	// for (auto i = 0; i < 3; ++i)
+	for (int elem : a) // like for each
+		// cout << a[i] << ", ";
+		cout << elem << ", ";
 		cout << endl;
 }
