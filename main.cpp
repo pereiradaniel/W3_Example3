2838eb54f9bb143ef84b0ef51cba63c9a4eb2dd6
pereiradaniel <danielpaulpereira@protonmail.com>
2021-01-25 12:42:46 -0500

    Example.

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main()
 {
-	int a[5]{ 1, 56, -3 };
+	int a[5] { 1, 56, -3 };
 
-	for (auto i = 0; i < 5; ++i)
+	for (auto i = 0; i < 3; ++i)
 		cout << a[i] << ", ";
-	cout << endl;
+		cout << endl;
 }
